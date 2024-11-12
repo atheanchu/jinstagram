4f0ae26a0f0306feb576e5c0ffbae20d5acd2edf
atheanchu <hyun.jin.hwan@gmail.com>
2024-11-12 17:18:47 +0900

    fix: remove frontend and resources directory to recreate them with better name

--- resources/JinstagramResources/bin/jinstagram_resources.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { JinstagramResourcesStack } from "../lib/jinstagram_resources-stack";
-
-const app = new cdk.App();
-new JinstagramResourcesStack(app, "JinstagramResourcesStack", {
-    env: { account: process.env.CDK_DEFULAT_ACCOUNT, region: "ap-northeast-2" },
-});
